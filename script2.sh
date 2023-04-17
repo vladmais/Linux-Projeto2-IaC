ec9408c3f3ac5aa1ce33e4ab159be3c0c57888eb
vladmais <79092475+vladmais@users.noreply.github.com>
2023-04-18 03:05:34 +1000

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Baixando e copiando arquivos da aplicação..."
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
